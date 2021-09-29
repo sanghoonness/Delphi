098540cccad1d1e3b4540672dd1343552f18abc3
sanghoon oh <sahoh88@naver.com>
2021-09-29 13:51:44 +0900

    구리한강시민공원 입차 프로그램

---
+++ 구리한강시민공원/출입모니터링/소스/SC_IO.dpr
@@ -0,0 +1,23 @@
+program SC_IO;
+
+uses
+  Forms,
+  Main in 'Main.pas' {frmMain},
+  Tables in 'Tables.pas' {dmTables: TDataModule},
+  Setup in 'Setup.pas' {frmSetup},
+  UnitInfo in 'UnitInfo.pas' {frmUnitInfo},
+  Global in 'Global.pas',
+  Login in 'Login.pas' {frmLogin},
+  InDspSet in 'InDspSet.pas' {frmInDspSet},
+  DspSet in 'DspSet.pas' {frmDspSet};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := '정기차량출입관리';
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TdmTables, dmTables);
+  Application.Run;
+end.
